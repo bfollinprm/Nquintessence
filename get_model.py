7c849785b3e9b46236f59d33093f3923868821cc
brent follin <brentfollin@gmail.com>
2014-07-07 15:49:34 -0700

    updated get_model with Matthews derived class solution

--- get_model.py
+++ get_model.py
@@ -21,7 +21,7 @@ def get_model(**CMB_parameters):
 	'chi0'
 	'chidot0'
 	'''
-	model = Class()
+	tmp_model = Class()
 	valid_params = [  ###Can find more in class explanatory.ini file.  These are the only ones we need, though.
 					'A_s',
 					'H0',
@@ -36,31 +36,50 @@ def get_model(**CMB_parameters):
 		if key in valid params:
 			params[key] = value
 
-	model.set({'output': 'lCl, tCl', 'lensing':'yes', 'l_max_scalars':4000})
-	model.set(params)
+	tmp_model.set({'output': 'lCl, tCl', 'lensing':'yes', 'l_max_scalars':4000})
+	tmp_model.set(params)
 
 	### Compute Background quantities
 
-	model.compute(lvl = ['background'])
+	tmp_model.compute(lvl = ['background'])
 
 	### Sub out the hubble module for one that includes the background effects of the chi field.
-	oldHubble = model.Hubble
+	oldHubble = tmp_model.Hubble
+	oldDa = tmp_model.angular_diameter_distance
+	del tmp_model
 	output, newHubble = get_hubble_rate(CMB_parameters)
+	redshift = linspace(0, output['z'].max(), output['z'].size)
+	newDa = cumsum(array([quad(lambda z: 1/newHubble(z) for i in arange(redshift.size - 1)]))* 1.0/(1+redshift)
 
-	def Hubble(z):
-		try:
-			newHubble(z)
-		else:
-			oldHubble(z) ##Revert to old history for z > z0
+	class NewClass(Class):
+		def Hubble(self, z):
+			try:
+				newHubble(z)
+			else:
+				oldHubble(z) ##Revert to old history for z > z0
+		def angular_diameter_distance(self, z):
+			try:
+				newDa(z)
+			else:
+				oldDa(z)
 
-	cmb.Hubble = Hubble
-
+	model = NewClass()
+	model.set({'output': 'lCl, tCl', 'lensing':'yes', 'l_max_scalars':4000})
+	model.set(params)
+	model.compute()
 	###this changes what H0 is
-	CMB_parameters['H0'] = hubble_rate(0) * 1e5
-
-	cmb.compute()
+	CMB_parameters['H0_true'] = model.Hubble(0) * 1e5
+	derived_parameters = ['age', 'z_reio', 'z_rec','rs_rec','100*theta_s', 'YHe', 'sigma8']
+	for key in derived_parameters:
+	CMB_parameters[key] = model.get_current_derived_parameters(derived_parameters)
+	CMB_parameters['age'] = model.age()
 	_ell = arange(3001)
-	clTT = cmb.lensed_cls(3000)['tt'] * _ell * (_ell + 1) * (1.0e6*cmb.Tcmb())**2
+	CMB_parameters['clTT'] = model.lensed_cls(3000)['tt'] * _ell * (_ell + 1) * (1.0e6*model.Tcmb())**2
 
+	###### ADD MORE REQUIRED NUMBERS HERE, e.g. below ######
+	CMB_parameters['H057'] = model.Hubble(0.57)
+	CMB_parameters['DA057'] = model.angular_diameter_distance(0.57)
+
+	return CMB_parameters
 
 	
